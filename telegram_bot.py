e32753e8947f30859bc4365ac39f602420d6d6f6
karfas <80702492+karfas01@users.noreply.github.com>
2025-01-15 20:01:13 +0300

    added by ifmain

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,17 +4,21 @@ import telegram
 import os
 
 
-load_dotenv()
+def main():
+    load_dotenv()
 
-tg_bot_key = os.getenv("TELEGRAM_BOT_TOKEN")
-tg_chanal_name = os.getenv("CHAT_ID")
+    tg_bot_key = os.getenv("TELEGRAM_BOT_TOKEN")
+    tg_chanal_name = os.getenv("CHAT_ID")
 
-bot = telegram.Bot(token=tg_bot_key)
+    bot = telegram.Bot(token=tg_bot_key)
+    try:
+        get_comic()
 
-try:
-    get_comic()
+        with open("comic_image.png", "rb") as f:
+            bot.send_photo(chat_id=tg_chanal_name, photo=f)
+    finally:
+        os.remove("comic_image.png")
 
-    with open("comic_image.png", "rb") as f:
-        bot.send_photo(chat_id=tg_chanal_name, photo=f)
-finally:
-    os.remove("comic_image.png")
+
+if __name__ == "__main__":
+    main()
